01f25786a8d7815ec892f3cce90ea4ab09afa74f
rohit <rohitlion5678@gmail.com>
2025-05-06 15:33:27 +0545

    add

--- app.py
+++ app.py
@@ -1,21 +1,12 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
 @app.route('/')
-def home():
-    return '<h1>Welcome to my Flask app! Learning never stops. - Rohit...........Biratnagar.</h1>'
-
-@app.route('/about')
-def about():
-    return '<h1>This is a simple web app built with Flask. Keep exploring...........Nepal.</h1>'
-
-@app.route('/contact')
-def contact():
-    return '<h1>Contact me anytime. Believe in yourself...........Rohit.</h1>'
-
-@app.route('/fun')
-def fun():
-    return '<h1>Coding is creative and fun. Keep smiling...........Kathmandu.</h1>'
+def add_numbers():
+    num1 = request.args.get('num1', default=0, type=float)
+    num2 = request.args.get('num2', default=0, type=float)
+    total = num1 + num2
+    return f'<h1>The sum of {num1} and {num2} is {total}...........Rohit.</h1>'
 
 app.run(host='0.0.0.0', port=7000)
