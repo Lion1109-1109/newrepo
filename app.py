f58ed3f3b5cec876e3a52a34c6e35b04bde010a6
rohit <rohitlion5678@gmail.com>
2025-05-06 14:30:53 +0545

    changes made

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ app=Flask(__name__)
 
 def webout():
 
- return '<h1>DevOps is so much fun to learn. we get success!!!</h1>'
+ return '<h1>DevOps is so much fun to learn. we get success!!!
+ </h1> <h2>I love programming </h2>'
 
 app.run(host='0.0.0.0',port=7000)
