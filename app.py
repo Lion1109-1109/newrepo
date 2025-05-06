08a0ec82c1cc3fb10d5ef4537f568a25cd61440c
rohit <rohitlion5678@gmail.com>
2025-05-06 14:47:47 +0545

    sum of two numbers

--- app.py
+++ app.py
@@ -6,6 +6,17 @@ app=Flask(__name__)
 
 def webout():
 
- return '<h1>DevOps is so much fun to learn. we get success!!! I love programming </h1>'
+ return '<h1># Add two numbers
+
+# Input from the user
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+# Add the numbers
+sum = num1 + num2
+
+# Display the result
+print("The sum is:", sum)
+</h1>'
 
 app.run(host='0.0.0.0',port=7000)
